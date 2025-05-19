8ec75ffa8f751288ef8d8d2ba366e2e82080a1a6
rjzim01 <rjzim01@gmail.com>
2025-05-19 14:04:00 +0600

    route file update to normal route from resourse rouce

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,26 @@ require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 
 
-Route::resource('projects', ProjectController::class);
+// Route::resource('projects', ProjectController::class);
+
+// Index - list all projects
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+
+// Create - show create form
+Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+
+// Store - handle form submission for creating a new project
+Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+
+// Show - display a single project
+Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+
+// Edit - show edit form for a project
+Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+
+// Update - handle form submission for updating a project
+Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+
+// Destroy - delete a project
+Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
 
